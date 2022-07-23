75ef09e9b0e84f5bfa81f9badfcf544d129ba2ba
maximdorogov <mdorogov@fi.uba.ar>
2022-07-23 00:41:16 -0300

    added support for multiple video streams when reading from folder

--- run_server.sh
+++ run_server.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 
-
-SOURCE_PATH=$(dirname $(realpath "$2"))
-FILE_NAME=$(basename "$2")
+SRC_TYPE=$1
+VIDEO_SRC=$2
+SOURCE_PATH=$(dirname $(realpath "$VIDEO_SRC"))
+FILE_NAME=$(basename "$VIDEO_SRC")
 PORT=$4
 
-echo $SOURCE_PATH
-echo $FILE_NAME
+if [ "$SRC_TYPE" = "-folder" ]; then
+    VIDEOS=`ls $VIDEO_SRC`
+    SOURCE_PATH=$(realpath "$VIDEO_SRC")
+else
+    VIDEOS=$FILE_NAME
+    SOURCE_PATH=$(dirname $(realpath "$VIDEO_SRC"))
+fi
 
-docker run --rm -v $SOURCE_PATH:/videos -p $PORT:8554 --name rtsp-server -d  rtsp-server
+docker run --rm -v $SOURCE_PATH:/videos -p $PORT:8554 --name rtsp-server -d  rtsp-server > /dev/null
 
+for VIDEO_FILE in $VIDEOS
+do
 docker exec -d rtsp-server ffmpeg -re \
     -stream_loop -1 \
-    -i ./videos/$FILE_NAME \
+    -i ./videos/$VIDEO_FILE \
     -c copy \
-    -f rtsp rtsp://localhost:8554/$FILE_NAME
+    -f rtsp rtsp://localhost:8554/$VIDEO_FILE
 
-echo "streaming on rtsp://localhost:$PORT/$FILE_NAME" 
+echo "streaming on rtsp://localhost:$PORT/$VIDEO_FILE"
+done
